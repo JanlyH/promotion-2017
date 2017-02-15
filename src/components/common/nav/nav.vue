e7f8b485318aed19406c6bbaf5ac9dd9e10c1453
Jan <346673304@qq.com>
2017-02-15 19:06:22 +0800

    用户信息

--- src/components/common/nav/nav.vue
+++ src/components/common/nav/nav.vue
@@ -13,16 +13,40 @@
                 <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect">
                     <el-menu-item index="1">卖家中心</el-menu-item>
                     <el-menu-item index="2">进入店铺</el-menu-item>
-                    <el-submenu index="3" >
-                        <template slot="title" >宝贝团公司</template>
-                    </el-submenu>
+                    <el-popover  placement="bottom-end" width="360" trigger="hover">
+                        <el-submenu index="3" slot="reference" >
+                            <template slot="title">宝贝团公司</template>
+                        </el-submenu>
+
+                        <!-- 用户信息 -->
+                        <div class="seller-warp">
+                            <div class="seller-info">
+                                <div class="seller-logo">
+                                    <img src="https://img.alicdn.com/imgextra/i2/17157061/TB2F929tVXXXXcLXXXXXXXXXXXX-17157061.jpg">
+                                </div>
+                                <div class="base-info">
+                                    <div class="seller-nick">宝贝团公司<i class="seller-tag">全能版</i></div>
+                                    <div class="due-time">
+                                        软件到期时间：2017-01-11<el-button type="primary" size="mini" class="renew-btn">续费</el-button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="btn-warp">
+                                <div class="btn-bar">
+                                    <el-button size="small">推荐朋友</el-button>
+                                    <el-button size="small">我的积分</el-button>
+                                    <el-button size="small">短信充值</el-button>
+                                </div>
+                                <div class="btn-bar">
+                                    <el-button size="small">软件设置</el-button>
+                                    <el-button size="small">子账号管理</el-button>
+                                    <el-button size="small">卖家学堂</el-button>
+                                </div>
+                            </div>
+                            <div class="sign-out">退出登录</div>
+                        </div>
+                    </el-popover>
                 </el-menu>
-                <div v-popover:sellerInfo>
-                    阿大声道
-                </div>
-                <el-popover ref="sellerInfo" placement="bottom" width="160" trigger="hover">
-                    <p>这是一段内容这是一段内容确定删除吗？</p>
-                </el-popover>
             </div>
         </div>
     </div>
@@ -30,12 +54,13 @@
 
 <script>
     import Vue from 'vue';
-    import {Menu, MenuItem, Submenu, Popover} from 'element-ui';
+    import {Menu, MenuItem, Submenu, Popover, Button} from 'element-ui';
     import sellerNav from 'components/common/nav/sellerNav';
     Vue.use(Menu);
     Vue.use(MenuItem);
     Vue.use(Submenu);
     Vue.use(Popover);
+    Vue.use(Button);
     export default {
         name: 'nav',
         methods: {
@@ -62,6 +87,66 @@
             }
             .el-menu-demo{
                 float: right;
+            }
+        }
+    }
+    .seller-warp{
+        .seller-info{
+            height: 90px;
+            width: 100%;
+            margin-bottom: 10px;
+        }
+        .seller-logo{
+            width: 90px;
+            height: 90px;
+            padding: 10px;
+            float: left;
+            margin-right: 20px;
+            img{
+                width: 100%;
+                height: 100%;
+                border-radius: 50%;
+            }
+        }
+        .base-info{
+            float: left;
+            font-size: 14px;
+            .seller-tag{
+                padding: 2px 4px;
+                margin-left: 12px;
+                // border-radius: 2px;
+                font-size: 12px;
+                background-color: #ff4949;
+                color: #fff;
+            }
+            .seller-nick{
+                margin: 16px 0 10px 0;
+            }
+            .renew-btn{
+                margin-left: 12px;
+            }
+        }
+        .btn-warp{
+            margin-bottom: 20px;
+            text-align: center;
+            .btn-bar{
+                margin-bottom: 8px;
+                button{
+                    width: 100px;
+                }
+            }
+        }
+        .sign-out{
+            float: right;
+            padding: 8px 10px;
+            background-color: #fff;
+            border-radius: 4px;
+            cursor: pointer;
+            text-align: center;
+            transition: all .3s;
+            &:hover{
+                background-color: #f5f5f5;
+                transition: all .3s;
             }
         }
     }
